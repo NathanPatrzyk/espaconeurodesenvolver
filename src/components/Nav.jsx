caddd21fb057f4c81bd4d3077df361018321c4e6
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-15 19:38:56 -0300

    2.1.1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,9 +8,9 @@ const Nav = () => {
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className="bg-white bg-opacity-70 md:px-8 px-4 py-3 w-full md:flex items-center fixed top-0 left-0">
+    <div className="bg-white bg-opacity-90 px-8 py-3 w-full md:flex items-center fixed top-0 left-0">
       <div className="flex items-center">
-        <div className="sm:w-72 w-10 h-10 bg-[url(./assets/logo-small.png)] sm:bg-[url(./assets/logo.png)] bg-no-repeat bg-center bg-contain cursor-pointer hover:opacity-80 focus:opacity-80 duration-500"></div>
+        <div className="sm:w-72 w-10 h-10 bg-[url(./assets/logo-small.webp)] sm:bg-[url(./assets/logo.webp)] bg-no-repeat bg-center bg-contain cursor-pointer hover:opacity-80 focus:opacity-80 duration-500"></div>
       </div>
       <div
         onClick={() => setOpen(!open)}
@@ -19,7 +19,7 @@ const Nav = () => {
         }`}
       ></div>
       <div
-        className={`bg-white md:bg-opacity-0 bg-opacity-100 md:p-0 p-2 pt-4 md:flex md:gap-4 md:justify-end absolute md:static md:z-auto z-[-1] left-0 w-full ${
+        className={`bg-white md:bg-opacity-0 bg-opacity-90 md:p-0 p-2 pt-4 md:flex md:gap-4 md:justify-end absolute md:static md:z-auto z-[-1] left-0 w-full ${
           open ? "top-[64px]" : "top-[-490px]"
         }`}
       >
