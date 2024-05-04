e1704af4e17c61606e2a8c44450a11ce6dab40c7
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-04 20:02:08 -0300

    main-3.1.3

--- src/components/LinkGoBack.jsx
+++ src/components/LinkGoBack.jsx
@@ -1,5 +1,5 @@
 const LinkGoBack = () => {
-  return <a className="text-cyan-neuro font-bold text-left underline block pt-[70px] pb-2 md:pt-24 md:pb-8 md:px-16 px-4" href="/">Voltar</a>;
+  return <a className="text-cyan-neuro font-bold text-left underline block mt-[70px] mb-2 md:mt-24 md:mb-8 md:mx-16 mx-4" href="/">Voltar</a>;
 };
 
 export default LinkGoBack;
