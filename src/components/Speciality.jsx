476f2e0356f6002bed1752ef1815e954b98efaa9
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-14 18:54:13 -0300

    version-5.1.0

--- src/components/Speciality.jsx
+++ src/components/Speciality.jsx
@@ -1,45 +1,80 @@
 import Link from "./Link";
 
 const Speciality = (props) => {
-  if (props.link) {
-    return (
-      <section className="flex md:flex-row flex-col bg-light-cyan-neuro">
-        <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-dark-cyan-neuro md:to-cyan-neuro bg-gradient-to-r from-transparent to-transparent md:rounded-none md:rounded-tr-[800px] rounded-none">
-          <img
-            src={props.image}
-            className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
-          ></img>
-        </div>
-        <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
-          <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
-            {props.title}
-          </h2>
-          <p className="text-dark-neuro py-6">{props.description}</p>
-          <Link
-            link={props.link}
-            name="SAIBA MAIS"
-            theme="dark"
-          />
-        </div>
-      </section>
-    );
-  } else {
-    return (
-      <section className="flex md:flex-row flex-col bg-light-cyan-neuro">
-        <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-dark-cyan-neuro md:to-cyan-neuro bg-gradient-to-r from-transparent to-transparent md:rounded-none md:rounded-tr-[800px] rounded-none">
-          <img
-            src={props.image}
-            className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
-          ></img>
-        </div>
-        <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
-          <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
-            {props.title}
-          </h2>
-          <p className="text-dark-neuro pt-6">{props.description}</p>
-        </div>
-      </section>
-    );
+  if (props.theme == "dark") {
+    if (props.link) {
+      return (
+        <section className="flex md:flex-row flex-col bg-light-cyan-neuro">
+          <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-dark-cyan-neuro md:to-cyan-neuro bg-gradient-to-r from-transparent to-transparent md:rounded-none md:rounded-tr-[800px] rounded-none">
+            <img
+              src={props.image}
+              className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
+            ></img>
+          </div>
+          <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
+            <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
+              {props.title}
+            </h2>
+            <p className="text-dark-neuro py-6">{props.description}</p>
+            <Link link={props.link} name="SAIBA MAIS" theme="dark" />
+          </div>
+        </section>
+      );
+    } else {
+      return (
+        <section className="flex md:flex-row flex-col bg-light-cyan-neuro">
+          <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-dark-cyan-neuro md:to-cyan-neuro bg-gradient-to-r from-transparent to-transparent md:rounded-none md:rounded-tr-[800px] rounded-none">
+            <img
+              src={props.image}
+              className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
+            ></img>
+          </div>
+          <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
+            <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
+              {props.title}
+            </h2>
+            <p className="text-dark-neuro pt-6">{props.description}</p>
+          </div>
+        </section>
+      );
+    }
+  } else if (props.theme == "light") {
+    if (props.link) {
+      return (
+        <section className="flex md:flex-row flex-col-reverse bg-light-neuro">
+          <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8 ">
+            <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
+              {props.title}
+            </h2>
+            <p className="text-dark-neuro pt-6">{props.description}</p>
+            <Link link={props.link} name="SAIBA MAIS" theme="dark" />
+          </div>
+          <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-light-cyan-neuro md:to-light-cyan-neuro md:rounded-none md:rounded-bl-[800px] rounded-none">
+            <img
+              src={props.image}
+              className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
+            ></img>
+          </div>
+        </section>
+      );
+    } else {
+      return (
+        <section className="flex md:flex-row flex-col-reverse bg-light-neuro">
+          <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8 ">
+            <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
+              {props.title}
+            </h2>
+            <p className="text-dark-neuro pt-6">{props.description}</p>
+          </div>
+          <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-light-cyan-neuro md:to-light-cyan-neuro md:rounded-none md:rounded-bl-[800px] rounded-none">
+            <img
+              src={props.image}
+              className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
+            ></img>
+          </div>
+        </section>
+      );
+    }
   }
 };
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,10 +7,9 @@ import ImagePsicologia from "../assets/psicologia.webp";
 import ImageFonoaudiologia from "../assets/fonoaudiologia.webp";
 
 import About from "../components/About";
-import Main from "../components/Main";
+import MainHome from "../components/MainHome";
 import Nav from "../components/Nav";
 import Speciality from "../components/Speciality";
-import SpecialityAlternative from "../components/SpecialityAlternative";
 import SocialMedia from "../components/SocialMedia";
 import Location from "../components/Location";
 import LinkGoToProfessionals from "../components/LinkGoToProfessionals";
@@ -19,7 +18,7 @@ import Footer from "../components/Footer";
 function Home() {
   return (
     <div>
-      <Main />
+      <MainHome />
       <div id="sobre">
         <About />
       </div>
@@ -29,6 +28,7 @@ function Home() {
           title="PediaSuit"
           description="É uma abordagem revolucionária na neuroreabilitação infantil, projetada para crianças com desafios neuromotores. Descubra como esse protocolo funciona e como ele pode fazer a diferença na vida de nossos pequenos pacientes!"
           link="especialidades/pediasuit"
+          theme="dark"
         />
       </div>
       <div id="contato">
@@ -39,22 +39,33 @@ function Home() {
         title="Bobath"
         description="O Conceito Neuroevolutivo Bobath é uma abordagem terapêutica desenvolvida a fim de facilitar o individuo com limitação para participação no cotidiano, devido a danos motores, (incluindo tônus muscular e padrões de movimento) funções sensoriais, perceptivas e cognitivas, resultantes dos distúrbios do SNC – Sistema Nervoso Central."
         link="especialidades/bobath"
+        theme="dark"
       />
       <Location />
       <Speciality
         image={ImageTerapiaOcupacional}
         title="Terapia Ocupacional"
         description="Às vezes, as maiores transformações na vida vêm de pequenos detalhes. A Terapia Ocupacional é um exemplo perfeito disso. Essa prática terapêutica foca em capacitar as pessoas a superar desafios diários, tornando pequenos passos em direção a uma vida mais plena e independente. Cada pequena vitória é um grande avanço."
+        theme="dark"
       />
       <LinkGoToProfessionals />
       <Speciality
         image={ImageEstimulacaoVisual}
         title="Estimulação Visual"
         description="É uma especialidade que visa desenvolver e melhorar a capacidade de acuidade visual por meio de atividades específicas que favorecem o sistema visual."
+        theme="dark"
       />
-      <SpecialityAlternative image={ImagePsicomotricidade} title="Psicomotricidade" />
-      <Speciality image={ImagePsicologia} title="Psicologia" />
-      <SpecialityAlternative image={ImageFonoaudiologia} title="Fonoaudiologia" />
+      <Speciality
+        image={ImagePsicomotricidade}
+        title="Psicomotricidade"
+        theme="light"
+      />
+      <Speciality image={ImagePsicologia} title="Psicologia" theme="dark" />
+      <Speciality
+        image={ImageFonoaudiologia}
+        title="Fonoaudiologia"
+        theme="light"
+      />
       <Nav />
       <Footer />
     </div>
