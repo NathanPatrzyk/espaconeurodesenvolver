33acffdeb6252dff337e96868f5e358302e62f1e
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-31 19:50:25 -0300

    version-6.1.0

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import Link from "./Link";
 
 import IconWhatsapp from "../assets/whatsapp.svg";
@@ -6,7 +8,12 @@ import ImageMain from "../assets/main.webp";
 const Main = () => {
   return (
     <main className="flex md:flex-row flex-col-reverse bg-light-cyan-neuro">
-      <div className="flex-1 flex flex-col justify-center md:pt-48 md:pb-32 md:px-16 px-4 py-8">
+      <motion.div
+        initial={{ y: 32, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex-1 flex flex-col justify-center md:pt-48 md:pb-32 md:px-16 px-4 py-8"
+      >
         <h1 className="text-dark-neuro text-3xl">
           Agende uma avaliação e venha{" "}
           <span className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
@@ -21,12 +28,15 @@ const Main = () => {
             theme="dark"
           />
         </div>
-      </div>
+      </motion.div>
       <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 md:pt-28 pt-24 py-8 md:bg-gradient-to-r md:from-dark-cyan-neuro md:to-cyan-neuro bg-gradient-to-r from-transparent to-transparent md:rounded-none md:rounded-bl-[800px] rounded-none">
-        <img
+        <motion.img
+          initial={{ y: 32, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
           src={ImageMain}
           className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
-        ></img>
+        ></motion.img>
       </div>
     </main>
   );
