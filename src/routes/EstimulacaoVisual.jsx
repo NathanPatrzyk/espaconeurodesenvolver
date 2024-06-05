2b938944b226817c63f8e98960445d578b47c01d
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-06-04 21:02:05 -0300

    6.1.3

--- src/routes/EstimulacaoVisual.jsx
+++ src/routes/EstimulacaoVisual.jsx
@@ -1,5 +1,6 @@
 import json from "../data.json";
 
+import Motion from "../components/Motion";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
@@ -18,111 +19,127 @@ import ImageEstimulacaoVisualAlternative2 from "../assets/estimulacaoVisualAlter
 const EstimulacaoVisual = () => {
   const name = "Estimulação Visual";
   return (
-    <div className="bg-light-cyan-neuro pt-16">
-      {json.pages.map(
-        (page) =>
-          page.childrens &&
-          page.childrens.map((children) =>
-            children.name == name ? (
-              <LinksSmall previous={children.previous} next={children.next} />
-            ) : null
-          )
-      )}
-      <main className="flex md:flex-row flex-col-reverse">
+    <div className="pt-16">
+      <div className="bg-light-cyan-neuro">
+        {json.pages.map(
+          (page) =>
+            page.childrens &&
+            page.childrens.map((children) =>
+              children.name == name ? (
+                <LinksSmall previous={children.previous} next={children.next} />
+              ) : null
+            )
+        )}
+      </div>
+      <main className="bg-light-cyan-neuro flex md:flex-row flex-col-reverse">
         <div className="flex-1 flex flex-col md:py-12 md:px-16 md:pt-4 py-8 px-4 pt-4">
-          <h1 className="text-dark-neuro text-3xl pb-4">
-            <span className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
-              Estimulação Visual:
-            </span>{" "}
-            Potencializando o Desenvolvimento Global da Visão
-          </h1>
-          <p>
-            É a especialidade responsável pela melhora na capacidade de acuidade
-            visual, ou seja, a nitidez da visão no dia a dia.
-          </p>
+          <Motion>
+            <h1 className="text-dark-neuro text-3xl pb-4">
+              <span className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
+                Estimulação Visual:
+              </span>{" "}
+              Potencializando o Desenvolvimento Global da Visão
+            </h1>
+            <p>
+              É a especialidade responsável pela melhora na capacidade de acuidade
+              visual, ou seja, a nitidez da visão no dia a dia.
+            </p>
+          </Motion>
         </div>
       </main>
-      <section className="flex md:flex-row flex-col gap-8 bg-light-neuro md:px-16 px-4 md:py-12 py-8">
-        <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
-          <img className="mx-auto w-20 h-20" src={IconZoom} />
-          <p className="text-dark-neuro pt-4">
-            Criação de novas conexões neurais, possibilitando o cérebro a ver,
-            mesmo que ocorram alterações nas funções visuais.
-          </p>
-        </div>
-        <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
-          <img className="mx-auto w-20 h-20" src={IconCamera} />
-          <p className="text-dark-neuro pt-4">
-            Auxílio no aprendizado e desenvolvimento global da visão.
-          </p>
-        </div>
-        <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
-          <img className="mx-auto w-20 h-20" src={IconGlasses} />
-          <p className="text-dark-neuro pt-4">
-            Indicada para qualquer indivíduo, independente da idade ou alteração
-            visual apresentada.
-          </p>
-        </div>
-      </section>
-      <LinkGoToProfessionals />
-      <section className="flex md:flex-row flex-col bg-light-neuro">
-        <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-light-cyan-neuro md:to-light-cyan-neuro md:rounded-none md:rounded-tr-[800px] rounded-none">
-          <img
-            src={ImageEstimulacaoVisualAlternative}
-            className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
-          ></img>
-        </div>
-        <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8 ">
-          <p className="text-dark-neuro pt-6">
-            As principais indicações para a Estimulação Visual são:{" "}
-            <ul className="list-disc font-bold text-dark-cyan-neuro flex flex-col gap-1 ps-8 pt-4">
-              <li>Estrabismo</li>
-              <li>Nistagmo</li>
-              <li>Dificuldades de Atenção e Foco</li>
-              <li>Ambliopia</li>
-              <li>Dificuldades de Acomodação Visual</li>
-              <li>Insuficiência de Convergência</li>
-              <li>Glaucoma</li>
-              <li>Astigmatismo</li>
-              <li>Distúrbio Visual Cortical</li>
-              <li>Olhar Baixo</li>
-              <li>Entre Outros</li>
-            </ul>
-          </p>
-        </div>
-      </section>
-      <section className="bg-light-neuro">
-        <div className="flex md:flex-row flex-col-reverse bg-light-cyan-neuro">
-          <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
-            <h1 className="text-dark-neuro text-2xl">
-            O Espaço Neurodesenvolver disponibiliza as melhores metodologias no processo de fisioterapia e reabilitação. Venha conhecer!
-            </h1>
-            <div className="pt-6">
-              <Link
-                icon={IconWhatsapp}
-                link="https://api.whatsapp.com/send?phone=554299732535"
-                name="ENTRE EM CONTATO"
-                theme="dark"
-              />
-            </div>
+      <Motion>
+        <section className="flex md:flex-row flex-col gap-8 md:px-16 px-4 md:py-12 py-8">
+          <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
+            <img className="mx-auto w-20 h-20" src={IconZoom} />
+            <p className="text-dark-neuro pt-4">
+              Criação de novas conexões neurais, possibilitando o cérebro a ver,
+              mesmo que ocorram alterações nas funções visuais.
+            </p>
           </div>
-          <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:rounded-none md:rounded-bl-[800px] rounded-none">
+          <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
+            <img className="mx-auto w-20 h-20" src={IconCamera} />
+            <p className="text-dark-neuro pt-4">
+              Auxílio no aprendizado e desenvolvimento global da visão.
+            </p>
+          </div>
+          <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
+            <img className="mx-auto w-20 h-20" src={IconGlasses} />
+            <p className="text-dark-neuro pt-4">
+              Indicada para qualquer indivíduo, independente da idade ou alteração
+              visual apresentada.
+            </p>
+          </div>
+        </section>
+      </Motion>
+      <LinkGoToProfessionals />
+      <section className="flex md:flex-row flex-col">
+        <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:bg-gradient-to-r md:from-light-cyan-neuro md:to-light-cyan-neuro md:rounded-none md:rounded-tr-[800px] rounded-none">
+          <Motion>
             <img
-              src={ImageEstimulacaoVisualAlternative2}
+              src={ImageEstimulacaoVisualAlternative}
               className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
             ></img>
+          </Motion>
+        </div>
+        <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8 ">
+          <Motion>
+            <p className="text-dark-neuro pt-6">
+              As principais indicações para a Estimulação Visual são:{" "}
+              <ul className="list-disc font-bold text-dark-cyan-neuro flex flex-col gap-1 ps-8 pt-4">
+                <li>Estrabismo</li>
+                <li>Nistagmo</li>
+                <li>Dificuldades de Atenção e Foco</li>
+                <li>Ambliopia</li>
+                <li>Dificuldades de Acomodação Visual</li>
+                <li>Insuficiência de Convergência</li>
+                <li>Glaucoma</li>
+                <li>Astigmatismo</li>
+                <li>Distúrbio Visual Cortical</li>
+                <li>Olhar Baixo</li>
+                <li>Entre Outros</li>
+              </ul>
+            </p>
+          </Motion>
+        </div>
+      </section>
+      <section>
+        <div className="flex md:flex-row flex-col-reverse bg-light-cyan-neuro">
+          <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
+            <Motion>
+              <h1 className="text-dark-neuro text-2xl">
+              O Espaço Neurodesenvolver disponibiliza as melhores metodologias no processo de fisioterapia e reabilitação. Venha conhecer!
+              </h1>
+              <div className="pt-6">
+                <Link
+                  icon={IconWhatsapp}
+                  link="https://api.whatsapp.com/send?phone=554299732535"
+                  name="ENTRE EM CONTATO"
+                  theme="dark"
+                />
+              </div>
+            </Motion>
+          </div>
+          <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:rounded-none md:rounded-bl-[800px] rounded-none">
+            <Motion>
+              <img
+                src={ImageEstimulacaoVisualAlternative2}
+                className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
+              ></img>
+            </Motion>
           </div>
         </div>
       </section>
-      {json.pages.map(
-        (page) =>
-          page.childrens &&
-          page.childrens.map((children) =>
-            children.name == name ? (
-              <LinksSmall previous={children.previous} next={children.next} />
-            ) : null
-          )
-      )}
+      <div className="bg-light-cyan-neuro ">
+        {json.pages.map(
+          (page) =>
+            page.childrens &&
+            page.childrens.map((children) =>
+              children.name == name ? (
+                <LinksSmall previous={children.previous} next={children.next} />
+              ) : null
+            )
+        )}
+      </div>
       <Nav />
       <Footer />
     </div>
--- src/routes/TerapiaOcupacional.jsx
+++ src/routes/TerapiaOcupacional.jsx
@@ -1,5 +1,6 @@
 import json from "../data.json";
 
+import Motion from "../components/Motion";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
@@ -16,82 +17,94 @@ import ImageTerapiaOcupacionalAlternative from "../assets/terapiaOcupacionalAlte
 const TerapiaOcupacional = () => {
   const name = "Terapia Ocupacional";
   return (
-    <div className="bg-light-cyan-neuro pt-16">
-      {json.pages.map(
-        (page) =>
-          page.childrens &&
-          page.childrens.map((children) =>
-            children.name == name ? (
-              <LinksSmall previous={children.previous} next={children.next} />
-            ) : null
-          )
-      )}
-      <main className="flex md:flex-row flex-col-reverse">
+    <div className="pt-16">
+      <div className="bg-light-cyan-neuro">
+        {json.pages.map(
+          (page) =>
+            page.childrens &&
+            page.childrens.map((children) =>
+              children.name == name ? (
+                <LinksSmall previous={children.previous} next={children.next} />
+              ) : null
+            )
+        )}
+      </div>
+      <main className="bg-light-cyan-neuro flex md:flex-row flex-col-reverse">
         <div className="flex-1 flex flex-col md:py-12 md:px-16 md:pt-4 py-8 px-4 pt-4">
-          <h1 className="text-dark-neuro text-3xl pb-4">
-            <span className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
-              Terapia Ocupacional:
-            </span>{" "}
-            Grandes Mudanças Através dos Pequenos Detalhes
-          </h1>
-          <p>
-            As maiores transformações na vida vêm de pequenos detalhes. A
-            Terapia Ocupacional é um exemplo perfeito disso.
-          </p>
+          <Motion>
+            <h1 className="text-dark-neuro text-3xl pb-4">
+              <span className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
+                Terapia Ocupacional:
+              </span>{" "}
+              Grandes Mudanças Através dos Pequenos Detalhes
+            </h1>
+            <p>
+              As maiores transformações na vida vêm de pequenos detalhes. A
+              Terapia Ocupacional é um exemplo perfeito disso.
+            </p>
+          </Motion>
         </div>
       </main>
-      <section className="flex md:flex-row flex-col gap-8 bg-light-neuro md:px-16 px-4 md:py-12 py-8">
-        <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
-          <img className="mx-auto w-20 h-20" src={IconStairs} />
-          <p className="text-dark-neuro pt-4">
-            Foco em capacitar as pessoas a superar desafios diários, tornando
-            pequenos passos em direção a uma vida mais plena e independente,
-            onde cada pequena vitória é um grande avanço.
-          </p>
-        </div>
-        <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
-          <img className="mx-auto w-20 h-20" src={IconSmile} />
-          <p className="text-dark-neuro pt-4">
-            Acreditamos no poder das pequenas conquistas e no impacto
-            significativo que a Terapia Ocupacional pode ter em sua vida.
-          </p>
-        </div>
-      </section>
+      <Motion>
+        <section className="flex md:flex-row flex-col gap-8 md:px-16 px-4 md:py-12 py-8">
+          <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
+            <img className="mx-auto w-20 h-20" src={IconStairs} />
+            <p className="text-dark-neuro pt-4">
+              Foco em capacitar as pessoas a superar desafios diários, tornando
+              pequenos passos em direção a uma vida mais plena e independente,
+              onde cada pequena vitória é um grande avanço.
+            </p>
+          </div>
+          <div className="flex-1 px-8 py-4 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
+            <img className="mx-auto w-20 h-20" src={IconSmile} />
+            <p className="text-dark-neuro pt-4">
+              Acreditamos no poder das pequenas conquistas e no impacto
+              significativo que a Terapia Ocupacional pode ter em sua vida.
+            </p>
+          </div>
+        </section>
+      </Motion>
       <LinkGoToProfessionals />
-      <section className="bg-light-neuro">
+      <section>
         <div className="flex md:flex-row flex-col-reverse bg-light-cyan-neuro">
           <div className="flex-1 flex flex-col justify-center md:py-32 md:px-16 px-4 py-8">
-            <h1 className="text-dark-neuro text-2xl">
-              Entre em contato com nossa equipe e realize uma avaliação, para
-              que a Terapia Ocupacional possa ajudar nas atividades diárias do
-              seu filho(a)!
-            </h1>
-            <div className="pt-6">
-              <Link
-                icon={IconWhatsapp}
-                link="https://api.whatsapp.com/send?phone=554299732535"
-                name="ENTRE EM CONTATO"
-                theme="dark"
-              />
-            </div>
+            <Motion>
+              <h1 className="text-dark-neuro text-2xl">
+                Entre em contato com nossa equipe e realize uma avaliação, para
+                que a Terapia Ocupacional possa ajudar nas atividades diárias do
+                seu filho(a)!
+              </h1>
+              <div className="pt-6">
+                <Link
+                  icon={IconWhatsapp}
+                  link="https://api.whatsapp.com/send?phone=554299732535"
+                  name="ENTRE EM CONTATO"
+                  theme="dark"
+                />
+              </div>
+            </Motion>
           </div>
           <div className="flex-1 flex items-center justify-center md:px-16 md:py-12 py-8 md:rounded-none md:rounded-bl-[800px] rounded-none">
-            <img
-              src={ImageTerapiaOcupacionalAlternative}
-              className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
-            ></img>
+            <Motion>
+              <img
+                src={ImageTerapiaOcupacionalAlternative}
+                className="sm:size-[300px] size-[90vw] max-w-[300px] max-h-[300px]"
+              ></img>
+            </Motion>
           </div>
         </div>
       </section>
-      {json.pages.map(
-        (page) =>
-          page.childrens &&
-          page.childrens.map((children) =>
-            children.name == name ? (
-              <LinksSmall previous={children.previous} next={children.next} />
-            ) : null
-          )
-      )}
+      <div className="bg-light-cyan-neuro">
+        {json.pages.map(
+          (page) =>
+            page.childrens &&
+            page.childrens.map((children) =>
+              children.name == name ? (
+                <LinksSmall previous={children.previous} next={children.next} />
+              ) : null
+            )
+        )}
+      </div>
       <Nav />
       <Footer />
     </div>
