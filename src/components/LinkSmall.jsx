5724fe2756757fa7832e10ad2992bae0491027ae
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-07-04 20:44:43 -0300

    8.0.1

--- src/components/LinkSmall.jsx
+++ src/components/LinkSmall.jsx
@@ -1,14 +1,16 @@
 import Motion from "./Motion";
 
+import { Link } from "react-router-dom";
+
 const LinkSmall = ({ textColor, name, link }) => {
   return (
     <Motion>
-      <a
+      <Link
         className={`text-${textColor} font-bold text-left underline inline-block my-2 md:my-8 md:mx-16 mx-4`}
-        href={link}
+        to={link}
       >
         {name}
-      </a>
+      </Link>
     </Motion>
   );
 };
