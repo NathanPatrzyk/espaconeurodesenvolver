2c2a7c60182bfd6dec460a5f9122fb706412efec
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-07-06 19:46:21 -0300

    7.1.1

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -8,11 +8,14 @@ const Professional = () => {
       <section className="flex flex-col gap-12 md:pb-12 pb-8 md:px-16 px-4 pt-0">
         {json.profissionals.map((profissional) => (
           <div className="flex-1 flex flex-col md:flex-row gap-6 p-4 md:p-8 border-4 border-dark-neuro hover:border-cyan-neuro border-opacity-20 rounded-lg">
-            <img
-              className="size-[80%] max-w-[200px] max-h-[200px] mx-auto"
-              src={profissional.image}
-            />
-            <div className="flex-1 sm:text-left text-center">
+            <figure className="size-[80%] max-w-[200px] max-h-[200px] mx-auto">
+              <img
+                className="size-[100%] max-w-[200px] max-h-[200px]"
+                src={profissional.image}
+                alt={profissional.name}
+              />
+            </figure>
+            <div className="flex-1 md:text-left text-center">
               <h2 className="font-bold bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro text-transparent bg-clip-text text-3xl">
                 {profissional.name}
               </h2>
@@ -22,12 +25,13 @@ const Professional = () => {
               <p className="text-dark-neuro text-2xl pb-4">
                 {profissional.description}
               </p>
-              <div className="flex sm:block flex-col">
-                {profissional.formations.map((formation) => (
-                  <p className="text-dark-neuro inline-block px-3 py-0.5 m-1 sm:mx-1 mx-auto bg-light-cyan-neuro rounded-full border-cyan-neuro border-[3px]">
-                    {formation.name}
-                  </p>
-                ))}
+              <div className="flex md:block flex-col">
+                {profissional.formations &&
+                  profissional.formations.map((formation) => (
+                    <p className="text-dark-neuro inline-block px-3 py-0.5 m-1 md:mx-1 mx-auto bg-light-cyan-neuro rounded-full border-cyan-neuro border-[3px]">
+                      {formation.name}
+                    </p>
+                  ))}
               </div>
             </div>
           </div>
