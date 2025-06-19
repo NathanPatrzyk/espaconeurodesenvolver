510c776826eb4fd55794adb32a2d8e9d38453960
Nathan Patrzyk <nathanpatzyk11@gmail.com>
2025-06-19 20:52:02 -0300

    13.0.3

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -1,42 +1,30 @@
 import json from "../data.json";
-
 import Motion from "./Motion";
 
 const Professional = () => {
   return (
     <Motion>
-      <section className="flex flex-col gap-12 px-4 pb-8 pt-0 md:px-16 md:pb-12">
-        {json.profissionals.map((profissional) => (
-          <div className="flex flex-1 flex-col gap-6 rounded-lg border-4 border-dark-neuro border-opacity-20 p-4 hover:border-cyan-neuro md:flex-row md:p-8">
-            <figure className="mx-auto size-[80%] max-h-[200px] max-w-[200px]">
-              <img
-                className="size-[100%] max-h-[200px] max-w-[200px]"
-                src={profissional.image}
-                alt={profissional.name}
-              />
-            </figure>
-            <div className="flex-1 text-center md:text-left">
-              <h2 className="bg-gradient-to-r from-dark-cyan-neuro to-cyan-neuro bg-clip-text text-3xl font-bold text-transparent">
+      <div className="flex w-screen justify-center">
+        <section className="flex w-full max-w-7xl flex-wrap gap-4 p-4 md:p-12">
+          {json.profissionals.map((profissional) => (
+            <div
+              key={profissional.name}
+              className="flex w-full flex-1 flex-col items-center justify-between p-4 sm:min-w-96"
+            >
+              <h2 className="max-w-full text-center font-decoration text-3xl text-dark-cyan-neuro">
                 {profissional.name}
               </h2>
-              <p className="pt-2 text-2xl font-bold text-dark-neuro">
+              <p className="max-w-full pt-2 text-center text-lg font-bold uppercase text-dark-neuro">
                 {profissional.role}
               </p>
-              <p className="pb-4 text-2xl text-dark-neuro">
+              <p className="max-w-full text-center text-lg uppercase text-dark-neuro">
                 {profissional.description}
               </p>
-              <div className="flex flex-col md:block">
-                {profissional.formations &&
-                  profissional.formations.map((formation) => (
-                    <p className="m-1 mx-auto inline-block rounded-full border-[3px] border-cyan-neuro bg-light-cyan-neuro px-3 py-0.5 text-dark-neuro md:mx-1">
-                      {formation.name}
-                    </p>
-                  ))}
-              </div>
+              <p></p>
             </div>
-          </div>
-        ))}
-      </section>
+          ))}
+        </section>
+      </div>
     </Motion>
   );
 };
--- src/routes/Professionals.jsx
+++ src/routes/Professionals.jsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 
 import Professional from "../components/Professional";
 import Motion from "../components/Motion";
+import LinkSmall from "../components/link/LinkSmall";
 import { useState, useEffect } from "react";
 
 function Professionals() {
@@ -19,9 +20,10 @@ function Professionals() {
   return (
     <div>
       <div
-        className={`flex w-screen justify-center bg-cover bg-bottom bg-no-repeat pt-16 ${loaded ? "bg-dark-cyan-neuro" : bgImage}`}
+        className={`flex w-screen flex-col justify-center bg-cover bg-bottom bg-no-repeat pt-16 ${loaded ? "bg-dark-cyan-neuro" : bgImage}`}
       >
-        <div className="flex min-h-48 items-center p-4 md:p-12">
+        <LinkSmall textColor="white" name="Voltar" link="/" />
+        <div className="flex h-full min-h-40 justify-center p-4 md:p-8">
           <Motion>
             <h1 className="text-center font-decoration text-4xl text-white md:text-left">
               Nossos Profissionais
