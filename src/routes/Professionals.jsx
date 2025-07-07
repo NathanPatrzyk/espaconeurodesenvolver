3dd507d53c515f82c828d10c0d7245cafe5c0a39
Nathan Patrzyk <nathanpatzyk11@gmail.com>
2025-07-07 20:55:29 -0300

    13.0.9

--- src/routes/Professionals.jsx
+++ src/routes/Professionals.jsx
@@ -8,14 +8,16 @@ import LinkSmall from "../components/link/LinkSmall";
 function Professionals() {
   return (
     <div>
-      <div className="flex w-full flex-col justify-center bg-dark-cyan-neuro bg-[url('/cloud.svg')] bg-cover bg-bottom bg-no-repeat pt-16">
-        <LinkSmall textColor="white" name="Voltar" link="/" />
-        <div className="flex h-full justify-center p-4 md:p-8">
-          <Motion>
-            <h1 className="pb-14 text-center font-decoration text-4xl text-white md:text-left">
-              Nossos Profissionais
-            </h1>
-          </Motion>
+      <div className="flex w-full flex-col items-center bg-dark-cyan-neuro bg-[url('/cloud.svg')] bg-cover bg-bottom bg-no-repeat pt-16">
+        <div className="flex w-full max-w-7xl flex-col flex-wrap justify-center gap-4 px-4 md:px-12">
+          <LinkSmall textColor="white" name="Voltar" link="/" />
+          <div className="flex h-full justify-center p-4 md:p-8">
+            <Motion>
+              <h1 className="pb-14 text-center font-decoration text-4xl text-white md:text-left">
+                Nossos Profissionais
+              </h1>
+            </Motion>
+          </div>
         </div>
       </div>
       <Professional />
